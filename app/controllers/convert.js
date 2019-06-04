7b2f2603fd632a984764c1e39e2737a59f0e9280
Adriano <desenvacc@gmail.com>
2019-06-04 15:17:42 -0300

    master

--- app/controllers/convert.js
+++ app/controllers/convert.js
@@ -2,6 +2,7 @@ let parser = require('xml2json');
 let ObjectID = require('mongodb').ObjectID;
 let xmldom = require('xmldom');
 let xpath = require('xpath');
+var prettyData = require("pretty-data")
 
 module.exports.xmlToJson =function(application, req, res) {
     if (Object.keys(req.files).length === 0) {
@@ -13,57 +14,45 @@ module.exports.xmlToJson =function(application, req, res) {
         reversible: true
     };
     let parser = new xmldom.DOMParser();
-
-    let root = parser.parseFromString(xmlTemplate.data.toString(), 'text/xml');
+    let SXML = prettyData.pd.xmlmin(xmlTemplate.data.toString());
+    let root = parser.parseFromString(SXML, 'text/xml');
 
     let nodes =xpath.select('//survey', root);
     let survey = {};
+    let questions = [];
     nodes.forEach(function (survey) {
         let surveyLength = survey.childNodes.length;
         for(let i =0; i < surveyLength; i++){
-            console.log(survey.childNodes[i].tagName);
             let tagName = survey.childNodes[i].tagName;
             if (tagName === "metaDataGroup"){
 
             } else if (tagName === "choiceGroup") {
 
             } else if (tagName === "questionPage") {
-                let questionPageLength = survey.childNodes[i].childNodes.length;
-                for(let j =0; j < questionPageLength; j++) {
-                    let header = "";
-                    if (tagName === "header"){
-                        header = survey.childNodes[i].childNodes[j].firstChild.data;
-                    } else if(tagName === "branch") {
-
-                    }
-
-                    console.log(survey.childNodes[i].childNodes[j])
+                let header = survey.childNodes[i].childNodes[0].tagName === "header" ? survey.childNodes[i].childNodes[0].firstChild.data : "";
+                let branchNode = survey.childNodes[i].childNodes[survey.childNodes[i].childNodes.length-1].tagName === "branch" ? survey.childNodes[i].childNodes[survey.childNodes[i].childNodes.length-1] : null;
+                let startNode = 0;
+                if(header !== ""){
+                    startNode = 1;
                 }
+                questions = getQuestions(questions,survey.childNodes[i].childNodes[startNode],header,branchNode);
+                console.log(questions)
             }
         }
     });
-
-
-    // let json = parser.toJson(xmlTemplate,options);
-    // let connection = application.config.dbConnection();
-    // connection.then(function(db) {
-    //     f(db,json);
-    // }, function(err) {
-    //     console.log(err);
-    // });
-    //
-    //
-    //
-    // function f(db,json) {
-    //     let convertModel = new application.app.models.ConversionDAO(db);
-    //     convertModel.saveXMLInJson(json, function(result) {
-    //         let oid = new ObjectID(result.insertedIds[0].id);
-    //         convertModel.buildModel(oid,function (result) {
-    //             result.forEach(elements=> {
-    //                 console.log(elements);
-    //             });
-    //             res.redirect('/');
-    //         })
-    //     });
-    // }
+    console.log(questions)
 };
+
+function getQuestions(questions,nextSibling,header,branchNode) {
+        if(nextSibling.tagName && nextSibling.tagName !== "branch" && nextSibling.tagName !== "header") {
+            questions.push(nextSibling.tagName);
+            if (nextSibling.tagName === "basicQuestionGroup") {
+                return getQuestions(questions, nextSibling.childNodes[0], header, branchNode);
+            } else if (nextSibling.nextSibling) {
+                return getQuestions(questions, nextSibling.nextSibling, header, branchNode);
+            } else if (nextSibling.parentNode.tagName === "basicQuestionGroup" && nextSibling.parentNode.nextSibling) {
+                return getQuestions(questions, nextSibling.parentNode.nextSibling, header, branchNode);
+            }
+        }
+        return questions
+}
