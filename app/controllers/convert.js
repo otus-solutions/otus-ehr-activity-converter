ff40c62f812df4316fa03ec3bedf48c4911e677a
Adriano <desenvacc@gmail.com>
2019-06-07 18:02:11 -0300

    OS-43 preenchimento de options da questão singleSelection

--- app/controllers/convert.js
+++ app/controllers/convert.js
@@ -18,9 +18,12 @@ let questionsDataTypeMap = new Map([
     ["DecimalQuestion","Decimal"],
     ["GridTextQuestion",null]
 ]);
-
+let choiceGroup;
 module.exports.xmlToJson =function(application, req, res) {
-    if (Object.keys(req.files).length === 0) {
+
+    choiceGroup = new Map();
+
+    if (!req.files || !req.files.template_XML) {
         return res.status(400).send('No files were uploaded.');
     }
 
@@ -58,10 +61,8 @@ module.exports.xmlToJson =function(application, req, res) {
         let surveyLength = survey.childNodes.length;
         for(let i =0; i < surveyLength; i++){
             let tagName = survey.childNodes[i].tagName;
-            if (tagName === "metaDataGroup"){
-
-            } else if (tagName === "choiceGroup") {
-
+            if (tagName === "choiceGroup") {
+                choiceGroup.set(survey.childNodes[i].attributes.getNamedItem("id").nodeValue,buildChoiceGroup(survey.childNodes[i]));
             } else if (tagName === "questionPage") {
                 let header = survey.childNodes[i].childNodes[0].tagName === "header" ? survey.childNodes[i].childNodes[0].firstChild.data : "";
                 let branchNode = survey.childNodes[i].childNodes[survey.childNodes[i].childNodes.length-1].tagName === "branch" ? survey.childNodes[i].childNodes[survey.childNodes[i].childNodes.length-1] : null;
@@ -70,53 +71,195 @@ module.exports.xmlToJson =function(application, req, res) {
                     startNode = 1;
                 }
                 questions = getQuestions(questions,survey.childNodes[i].childNodes[startNode],header,branchNode);
-                console.log(questions)
             }
         }
     });
     surveyJson.itemContainer = questions;
-    console.log(surveyJson)
+    res.status(200).render("home/index");
 };
+
+function buildChoiceGroup(ehrChoiceGroup) {
+    let choiceGroup = [];
+    let ehrChoiceGroupLength = ehrChoiceGroup.childNodes.length - 1;
+    for(let i = 0; i < ehrChoiceGroupLength; i++){
+        choiceGroup.push({
+            "extents" : "StudioObject",
+            "objectType" : "AnswerOption",
+            "dataType" : "Integer",
+            "label" : {
+                "ptBR" : {
+                    "extends" : "StudioObject",
+                        "objectType" : "Label",
+                        "oid" : "",
+                        "plainText" : ehrChoiceGroup.childNodes[i].attributes.getNamedItem("label").nodeValue,
+                        "formattedText" : ehrChoiceGroup.childNodes[i].attributes.getNamedItem("label").nodeValue
+                },
+                "enUS" : {
+                    "extends" : "StudioObject",
+                        "objectType" : "Label",
+                        "oid" : "",
+                        "plainText" : "",
+                        "formattedText" : ""
+                },
+                "esES" : {
+                    "extends" : "StudioObject",
+                        "objectType" : "Label",
+                        "oid" : "",
+                        "plainText" : "",
+                        "formattedText" : ""
+                }
+            },
+                "value" : i+1,
+                "extractionValue" : ehrChoiceGroup.childNodes[i].attributes.getNamedItem("value").nodeValue
+            })
+    }
+
+    return choiceGroup
+}
 
 function getQuestions(questions,nextSibling,header,branchNode) {
         if(nextSibling.tagName && nextSibling.tagName !== "branch" && nextSibling.tagName !== "header") {
             if (nextSibling.tagName === "basicQuestionGroup") {
                 return getQuestions(questions, nextSibling.childNodes[0], header, branchNode);
-            } else if (nextSibling.nextSibling) {
+            }
+
+            if (nextSibling.tagName === "booleanQuestion") {
+                let lastQuestionPosition = questions.length-1;
+                if(questions.length > 0 && questions[lastQuestionPosition].objectType === "CheckboxQuestion"){
+                    questions[lastQuestionPosition].options.push(buildOptionStructure(nextSibling));
+                } else {
+                    questions.push(buildQuestionItem(nextSibling));
+                }
+            } else {
                 questions.push(buildQuestionItem(nextSibling));
+            }
+
+            if (nextSibling.nextSibling) {
                 return getQuestions(questions, nextSibling.nextSibling, header, branchNode);
             } else if (nextSibling.parentNode.tagName === "basicQuestionGroup" && nextSibling.parentNode.nextSibling) {
-                questions.push(buildQuestionItem(nextSibling));
                 return getQuestions(questions, nextSibling.parentNode.nextSibling, header, branchNode);
             }
         }
         return questions
 }
 
+function buildOptionStructure(option) {
+    return {
+        "extents": "StudioObject",
+        "objectType": "CheckboxAnswerOption",
+        "optionID": option.attributes.getNamedItem("id").nodeValue,
+        "customOptionID": option.attributes.getNamedItem("id").nodeValue,
+        "dataType": "Boolean",
+        "label": {
+            "ptBR": {
+                "extends": "StudioObject",
+                "objectType": "Label",
+                "oid": "",
+                "plainText": fixString(option.attributes.getNamedItem("label").nodeValue),
+                "formattedText": fixString(option.attributes.getNamedItem("label").nodeValue)
+            },
+            "enUS": {
+                "extends": "StudioObject",
+                "objectType": "Label",
+                "oid": "",
+                "plainText": "",
+                "formattedText": ""
+            },
+            "esES": {
+                "extends": "StudioObject",
+                "objectType": "Label",
+                "oid": "",
+                "plainText": "",
+                "formattedText": ""
+            }
+        }
+    }
+}
+
 function buildQuestionItem(question){
-    let objectType = "";
+    let questionItem = {};
+    questionItem.objectType = "";
+    questionItem.label = buildQuestionLabel(question);
+    questionItem.metadata = {
+        "extents": "StudioObject",
+            "objectType": "MetadataGroup",
+            "options": []
+    };
+    if (question.attributes.getNamedItem("metaDataGroupId")){
+        questionItem.metadata.options = [{"extends":"StudioObject","objectType":"MetadataAnswer","dataType":"Integer","value":1,"extractionValue":".Q","label":{"ptBR":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"Não quer responder","formattedText":"Não quer responder"},"enUS":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""},"esES":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""}}},{"extends":"StudioObject","objectType":"MetadataAnswer","dataType":"Integer","value":2,"extractionValue":".S","label":{"ptBR":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"Não sabe","formattedText":"Não sabe"},"enUS":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""},"esES":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""}}},{"extends":"StudioObject","objectType":"MetadataAnswer","dataType":"Integer","value":3,"extractionValue":".A","label":{"ptBR":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"Não se aplica","formattedText":"Não se aplica"},"enUS":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""},"esES":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""}}},{"extends":"StudioObject","objectType":"MetadataAnswer","dataType":"Integer","value":4,"extractionValue":".F","label":{"ptBR":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"Não há dados","formattedText":"Não há dados"},"enUS":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""},"esES":{"extends":"StudioObject","objectType":"Label","oid":"","plainText":"","formattedText":""}}}]
+    }
+    questionItem.fillingRules = buildFillingRules(question);
+    questionItem.extents = "SurveyItem";
+    questionItem.templateID = question.attributes.getNamedItem("id").nodeValue;
+    questionItem.customID = question.attributes.getNamedItem("id").nodeValue;
+    questionItem.QuestionName = question.attributes.getNamedItem("name").nodeValue;
+
     if(question.tagName === "numericQuestion" ){
-        objectType = (question.attributes.getNamedItem("decimalNumber") && question.attributes.getNamedItem("decimalNumber").nodeValue === "false") ? "IntegerQuestion" : "DecimalQuestion"
+        questionItem.objectType = (question.attributes.getNamedItem("decimalNumber") && question.attributes.getNamedItem("decimalNumber").nodeValue === "false") ? "IntegerQuestion" : "DecimalQuestion"
     } else if(question.tagName === "textQuestion"){
-        objectType = "TextQuestion"
+        questionItem.objectType = "TextQuestion"
     } else if(question.tagName === "singleSelectionQuestion"){
-        objectType = "SingleSelectionQuestion";
+        questionItem.options = choiceGroup.get(question.attributes.getNamedItem("choiceGroupId").nodeValue);
+        questionItem.objectType = "SingleSelectionQuestion";
     } else if(question.tagName === "autocompleteQuestion"){
-        objectType = "AutocompleteQuestion";
+        questionItem.objectType = "AutocompleteQuestion";
     } else if(question.tagName === "dateQuestion"){
-        objectType = "CalendarQuestion";
+        questionItem.objectType = "CalendarQuestion";
+    }else if(question.tagName === "booleanQuestion"){
+        questionItem.objectType = "CheckboxQuestion";
+        questionItem.label = "";
+        questionItem.options = [];
+        questionItem.options.push(buildOptionStructure(question));
+        questionItem.templateID ="CheckboxQuestion" + questionItem.templateID;
+        questionItem.customID = "CheckboxQuestion" + questionItem.customID;
+
     } else {
-        objectType=question.tagName;
+        questionItem.objectType=question.tagName;
+    }
+
+    questionItem.dataType = questionsDataTypeMap.get(questionItem.objectType);
+
+    return questionItem;
+}
+
+function buildQuestionLabel(question){
+    let header = question.parentNode.firstChild.tagName === "header" ? question.parentNode.firstChild.firstChild.data : "";
+    let questionLabel = question.attributes.getNamedItem("label").nodeValue;
+    let plainText = "";
+    let formattedText = "";
+
+    if (question.tagName === "booleanQuestion" && header !== ""){
+
+    } else if(header !== ""){
+        plainText = header + questionLabel;
+        formattedText ="<div>" + header + "<br>" + questionLabel + "</div>"
+    } else {
+        plainText =  questionLabel;
+        formattedText = "<div>"+questionLabel+"</div>"
     }
 
     return {
-        "fillingRules": buildFillingRules(question),
-        "extents" : "SurveyItem",
-        "objectType" : objectType,
-        "templateID" : question.attributes.getNamedItem("id").nodeValue,
-        "customID" : question.attributes.getNamedItem("id").nodeValue,
-        "QuestionName" : question.attributes.getNamedItem("name").nodeValue,
-        "dataType" : questionsDataTypeMap.get(objectType)
+        "ptBR" : {
+        "extends" : "StudioObject",
+                "objectType" : "Label",
+                "oid" : "",
+                "plainText" : fixString(plainText),
+                "formattedText" : fixString(formattedText)
+        },
+        "enUS" : {
+        "extends" : "StudioObject",
+                "objectType" : "Label",
+                "oid" : "",
+                "plainText" : "",
+                "formattedText" : ""
+        },
+        "esES" : {
+        "extends" : "StudioObject",
+                "objectType" : "Label",
+                "oid" : "",
+                "plainText" : "",
+                "formattedText" : ""
+        }
     }
 }
 
@@ -180,6 +323,11 @@ function buildFillingRules(question){
 }
 
 function fixString(string){
-    string = string.replace(/([[][/]i])([/]i])/g, "</i>","<i>");
+    string = string
+        .replace(/\[i\]/g,"<i>")
+        .replace(/\[\/i\]/g,"</i>")
+        .replace(/\[b\]/g,"<b>")
+        .replace(/\[\/b\]/g,"</b>")
+        .replace(/\[br\]/g,"<br>");
     return string;
 }
