892b0c86e019c90e3bbb181aa1cafd808dd46047
FlaviaAP <flavia.avila@ufrgs.br>
2020-01-29 10:10:22 -0300

    OTUS-648 refatoradas classes Branch e Route

--- ehr2otus-groups/code/Branch.js
+++ ehr2otus-groups/code/Branch.js
@@ -1,47 +1,14 @@
-const globalVars = require('./globalVars');
 const Rule = require('./Rule');
-const NavigationHandler = require('./NavigationHandler');
 
-/* Its a route in Otus Template */
 class Branch {
 
     constructor(questionPageId, ehrBranch) {
         this.originPageId = questionPageId;
-        this.originId = "";
         this.targetPageId = ehrBranch.targetPageId;
-        this.targetId = "";
         this.rules = [];
         for(let rule of ehrBranch.rule){
             this.rules.push(new Rule(rule));
         }
-    }
-
-    extractExpressionsWithQuestionId(questionId){
-        return this.rules.filter(expr => expr.questionId === questionId);
-    }
-
-    setOrigin(originQuestionId){
-        if(this.originId === ""){
-            this.originId = originQuestionId;
-        }
-    }
-
-    setOriginAndTargetQuestionIds(originQuestionId, targetQuestionId){
-        this.setOrigin(originQuestionId);
-        if(this.targetPageId === globalVars.END_PAGE_ID){
-            this.targetId = globalVars.DEFAULT_NODES.END.id;
-        }
-        else{
-            this.targetId = targetQuestionId;
-        }
-    }
-
-    toOtusTemplate(){
-        let conditions = [];
-        for (let i = 0; i < this.rules.length; i++) {
-            conditions.push(this.rules[i].toOtusTemplate(i));
-        }
-        return NavigationHandler._getRouteObject(this.origin, this.target, false, conditions);
     }
 
 }
--- ehr2otus-groups/code/Route.js
+++ ehr2otus-groups/code/Route.js
@@ -6,6 +6,15 @@ class Route {
         this.destination = destinyQuestion;
         this.conditions = conditionsArr;
     }
+
+    toOtusTemplate(){
+        let conditions = [];
+        for (let i = 0; i < this.conditions.length; i++) {
+            conditions.push(this.conditions[i].toOtusTemplate(i)); //TODO
+        }
+        const isDefault = (conditions.length === 0);
+        return NavigationHandler._getRouteObject(this.origin, this.destination, isDefault, conditions);
+    }
 }
 
 module.exports = Route;
