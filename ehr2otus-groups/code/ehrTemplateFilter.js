59b80f493c60174fbde3a06d11234143e20513d1
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-07 12:42:53 -0300

    OTUS-648 removida variavel nao usada

--- ehr2otus-groups/code/ehrTemplateFilter.js
+++ ehr2otus-groups/code/ehrTemplateFilter.js
@@ -1,7 +1,6 @@
 const TAG_SEPARATOR = "_";
 
 let basicQuestionGroupId = [];
-let basicQuestionGroupCounter = 0;
 
 function extractQuestionsFromBasicGroup(questionArr, outputQuestions){
     for(let [key, subQuestionArr]  of questionArr){
@@ -9,10 +8,8 @@ function extractQuestionsFromBasicGroup(questionArr, outputQuestions){
         if(key === 'basicQuestionGroup'){
             for(let question of subQuestionArr){
                 basicQuestionGroupId.push(question.id);
-                basicQuestionGroupCounter++;
                 const subSubQuestionArr = Object.entries(question).filter(([key,value]) => key.includes('Question'));
                 extractQuestionsFromBasicGroup(subSubQuestionArr, outputQuestions);
-                basicQuestionGroupCounter--;
                 basicQuestionGroupId.pop();
             }
         }
@@ -54,7 +51,7 @@ function checkHiddenQuestions(questions){
             }
 
             if(j !== i+1){
-                console.log(`ATTENTION: question ${i} (${questions[i].id}) hide ${j} (${questions[j].id})`);
+                console.log(`ATTENTION: question index=${i} (${questions[i].id}) hide question index=${j} (${questions[j].id})`);
             }
         }
     }
